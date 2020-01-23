bbe3d50aab84aab069465b5ebfb11b61a9f6e607
atdtn <pavel.sab@gmail.com>
2020-01-23 05:05:26 +0200

    Update App.js
    Commit check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ready for CI/CD deploy ;)
+          Ready for CI/CD deploy ;) Commit check
         </p>
         <a
           className="App-link"
